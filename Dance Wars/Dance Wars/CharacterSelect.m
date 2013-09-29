48e1f37382d9ecd198281703399ce18f01ac183f
donsalari <ameyakoshti@gmail.com>
2013-09-28 18:13:43 -0700

    4 dudes added with frames and player selection

--- Dance Wars/Dance Wars/CharacterSelect.m
+++ Dance Wars/Dance Wars/CharacterSelect.m
@@ -44,18 +44,39 @@
         
         NSString *charPath1 = @"dance5.png";
         [self addSprite:charPath1];
-        ch.character.position = ccp(windowSize.width/4, windowSize.height*2/3);
+        ch.character.position = ccp(128,300);
         [self addChild:ch.character];
         
         charPath1 = @"dance1.png";
         [self addSprite:charPath1];
-        ch.character.position = ccp(windowSize.width/2, windowSize.height*2/3);
+        ch.character.position = ccp(384,300);
         [self addChild:ch.character];
         
+        charPath1 = @"dance2.png";
+        [self addSprite:charPath1];
+        ch.character.position = ccp(640,300);
+        [self addChild:ch.character];
+             
+        charPath1 = @"dance4.png";
+        [self addSprite:charPath1];
+        ch.character.position = ccp(896,300);
+        [self addChild:ch.character];
+     
         [[[CCDirector sharedDirector] touchDispatcher] addTargetedDelegate:self priority:0 swallowsTouches:YES];
 
     }
     return self;
+}
+
+-(void)draw
+{
+    [super draw];
+    
+    ccDrawColor4B(255, 255, 255, 255);
+    glLineWidth(5.0f);
+    ccDrawLine(ccp(256, 0), ccp(256, 768));
+    ccDrawLine(ccp(512, 0), ccp(512, 768));
+    ccDrawLine(ccp(768, 0), ccp(768, 768));
 }
 
 - (CCSprite *) addSprite:(NSString *)spritePath {
@@ -68,9 +89,7 @@
     
     [charSpriteList addObject:ch];
     NSLog(@"Bx%f,By%f",ch.character.boundingBox.origin.x, ch.character.boundingBox.origin.y);
-
-    
-    
+   
     return ch.character;
 }
 
@@ -90,9 +109,42 @@
         //NSLog(@"%@",[charSpriteList objectAtIndex:i]);
         NSLog(@"Bx%f,By%f,Lx%f,Ly%f",ch2.character.boundingBox.origin.x, ch2.character.boundingBox.origin.y, location.x, location.y);
         
-        if(CGRectContainsPoint(ch2.character.boundingBox, location)) {
         
+
+        
+        
+        //if(CGRectContainsPoint(ch2.character.boundingBox, location)) {
+        
+        // creating manual frames for individual players
+        
+        // Frame for player 1 selection
+        
+        if(location.x > 0 && location.x < 256 && location.y > 0 && location.y <768){
             [ch2 setSelected:@"1"];
+            CCScene *levelSelect = [LevelSelectLayer scene];
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:0.5 scene:levelSelect]];
+        }
+        
+        // Frame for player 2 selection
+        
+        if(location.x > 256 && location.x < 512 && location.y > 0 && location.y <768){
+            [ch2 setSelected:@"2"];
+            CCScene *levelSelect = [LevelSelectLayer scene];
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:0.5 scene:levelSelect]];
+        }
+        
+        // Frame for player 3 selection
+        
+        if(location.x > 512 && location.x < 768 && location.y > 0 && location.y <768){
+            [ch2 setSelected:@"3"];
+            CCScene *levelSelect = [LevelSelectLayer scene];
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:0.5 scene:levelSelect]];
+        }
+        
+        // Frame for player 4 selection
+        
+        if(location.x > 768 && location.x < 1024 && location.y > 0 && location.y <768){
+            [ch2 setSelected:@"4"];
             CCScene *levelSelect = [LevelSelectLayer scene];
             [[CCDirector sharedDirector] replaceScene:[CCTransitionFlipX transitionWithDuration:0.5 scene:levelSelect]];
         }
